8fff548c241b704a5fc046b9d55067bd134cf4a3
Brady <brady.savarie@hotmail.com>
2022-12-29 01:21:12 -0500

    Complete writing the javascript, modify some css properties, and rewrite the readme file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # etch-a-sketch
+
+This etch-a-sketch app was the second project that i've created since learning to program, and the main goal was to improve my DOM manipulation skills. It definitely helped me get a handle on modifying html elements via javascript, and I also learned how important planning is to the programming workflow. I started writing the script before I had a solid idea of how all of the features were going to work together, and while I did manage to make it work the result was a difficult to read, tough to maintain program. Once I realized this I did more research and ended up reverse-engineering another persons code. It was broken down in a way that made alot more sense and would make it significantly easier to add additional features or make any changes, so I decided to completely revamp my own using those same concepts. The next project I make I intend on staying in the planning phase longer (maybe even trying out flow charts) so that I can avoid writing myself into a corner again.
+
+I learned that developing software seems to flow best when following these steps:
+    1. Plan out the app very well. Use pseudocode, flow charts, design software and anything else that helps break down the big picture problems into smaller, managable steps.
+    2. Write the html out in full so that the structure of the webpage is well-defined.
+    3. Write the CSS. I have found that adding in the visual design at this point helps build motivation since you can visually see alot of progress happening quickly.
+    4. Write the javascript. I like to leave the logic for last because it's more exciting to see each feature come to life when it is in the context of good design. It also forces you to spend more time looking at the final design concept which could spark creative ideas to improve upon it.
+    5. Review the code for inefficiencies, trying to poke as many holes in it as possible so no learning experiences are wasted. Question every decision at least once.
--- script.js
+++ script.js
@@ -1,40 +1,111 @@
-const gridSize = document.getElementById('gridsizeslider');
+const defaultColor = '#000000';
+const defaultMode = 'colorMode';
+const defaultSize = 33;
+
+let currentColor = defaultColor;
+let currentMode = defaultMode;
+let currentSize = defaultSize;
+
+function setCurrentColor(newColor) {
+    currentColor = newColor;
+};
+
+function setCurrentMode(newMode) {
+    activateButton(newMode);
+    currentMode = newMode;
+};
+
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+};
+
+const colorSelector = document.getElementById('colorselectorinput');
+const colorModeBtn = document.getElementById('colormode');
+const rainbowModeBtn = document.getElementById('rainbowmode');
+const eraserBtn = document.getElementById('eraser');
+const shakeBtn = document.getElementById('shakebutton');
+const sizeLabel = document.getElementById('gridsizelabel');
+const sizeValue = document.getElementById('gridsizeslider');
 const grid = document.getElementById('grid');
-populateGrid(gridSize);
 
-// Update gridSize value on change
-gridSize.addEventListener('change', function() {
-    gridSize.setAttribute('value', gridSize.value);
-    populateGrid(gridSize);
-});
-// Replace text content of grid size label on input update
-gridSize.addEventListener('input', function() {
-    document.getElementById('gridsizelabel').textContent = `Grid Size: ${gridSize.value} x ${gridSize.value}`;
-});
+colorSelector.addEventListener('input', (e) => setCurrentColor(e.target.value));
+colorModeBtn.addEventListener('click', () => setCurrentMode('colorMode'));
+rainbowModeBtn.addEventListener('click', () => setCurrentMode('rainbowMode'));
+eraserBtn.addEventListener('click', () => setCurrentMode('eraser'));
+shakeBtn.addEventListener('click', () => reloadGrid());
+sizeValue.addEventListener('change', (e) => changeSize(e.target.value));
+sizeValue.addEventListener('input', (e) => updateSizeLabel(e.target.value));
 
-// Create cells based on gridSize and append to grid
-function populateGrid(gridSize) {
+let mouseDown = false;
+grid.addEventListener('mousedown', () => (mouseDown = true));
+grid.addEventListener('mouseup', () => (mouseDown = false));
+
+function changeSize(value) {
+    setCurrentSize(value);
+    updateSizeLabel(value);
+    reloadGrid();
+}
+
+function updateSizeLabel(value) {
+    sizeLabel.innerHTML = `Grid Size: ${value} x ${value}`;
+}
+
+function reloadGrid() {
     clearGrid();
-    for (let i = 1; i <= gridSize.value; i++) {
-        const row = document.createElement('div');
-        grid.appendChild(row);
-
-        for (let j = 1; j <= gridSize.value; j++) {
-            const cell = document.createElement('div');
-            cell.setAttribute('id', 'cell');
-            const height = grid.clientHeight;
-            const width = grid.clientWidth;
-            cell.style.width = `${width / gridSize.value}px`;
-            cell.style.height = `${height / gridSize.value}px`;
-            cell.style.border = "1px solid black";
-            row.appendChild(cell);
-        }
-    }
+    setupGrid(currentSize);
 }
 
 function clearGrid() {
-    grid.innerHTML = "";
+    grid.innerHTML = '';
 }
 
+function setupGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-// once the divs are hooked up i will add in the click listener functionality to change background color. I think the best route forward is for the colors to change when the mouse is clicked down and once released the functionality ends (?). hover is too frustrating to control. 
+    for (i = 1; i <= size * size; i++) {
+        const cell = document.createElement('div');
+        cell.addEventListener('mousedown', changeColor);
+        cell.addEventListener('mouseover', changeColor);
+        grid.appendChild(cell);
+    }
+}
+
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return;
+    if (currentMode === 'colorMode') {
+        e.target.style.backgroundColor = currentColor;
+    }
+    else if (currentMode === 'rainbowMode') {
+        const randomR = Math.floor(Math.random() * 256);
+        const randomG = Math.floor(Math.random() * 256);
+        const randomB = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+    }
+    else if (currentMode === 'eraser') {
+        e.target.style.backgroundColor = '#FFFFFF';
+    }
+}
+
+function activateButton(newMode) {
+    if (currentMode === 'colorMode') {
+        colorModeBtn.classList.remove('active')
+    } else if (currentMode === 'rainbowMode') {
+        rainbowModeBtn.classList.remove('active')
+    } else if (currentMode === 'eraser') {
+        eraserBtn.classList.remove('active');
+    };
+
+    if (newMode === 'colorMode') {
+        colorModeBtn.classList.add('active')
+    } else if (newMode === 'rainbowMode') {
+        rainbowModeBtn.classList.add('active')
+    } else if (newMode === 'eraser') {
+        eraserBtn.classList.add('active')
+    };
+}
+
+window.onload = () => {
+    setupGrid(defaultSize);
+    activateButton(defaultMode);
+  }
